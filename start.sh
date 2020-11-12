ce506971fcfadf9107365785f857e10284b48459
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-12 18:03:16 +0100

    Create a Django project: start.sh script

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Create a Django project
+# This instructs Compose to run django-admin startproject `docker-compose-django-postgresql` in a container, 
+# using the web service’s image and configuration.
+# Because the web image doesn’t exist yet, Compose builds it from the current directory, 
+# as specified by the build: . line in docker-compose.yml.
+# Once the web service image is built, Compose runs it and executes the django-admin startproject command
+# in the container.
+sudo docker-compose run web django-admin startproject docker-compose-django-postgresql .
